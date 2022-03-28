a60badca7bd522e2eba3ea341e9308d9c67c0704
sztamas123 <szalma_tamas@yahoo.com>
2022-03-28 17:57:53 +0300

    Added to git

---
+++ spring-boot-webshop/src/main/java/com/finalexam/webshop/Config/MyDataRestConfig.java
@@ -0,0 +1,40 @@
+package com.finalexam.webshop.Config;
+
+import com.finalexam.webshop.Entity.Product;
+import com.finalexam.webshop.Entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import javax.persistence.metamodel.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.DELETE, HttpMethod.POST};
+
+        config.getExposureConfiguration().forDomainType(Product.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+
+        config.getExposureConfiguration().forDomainType(ProductCategory.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+
+        //expose ids to show them in response
+        config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream().map(Type::getJavaType).toArray(Class[]::new));
+    }
+}
---
+++ spring-boot-webshop/src/main/java/com/finalexam/webshop/SpringBootWebshopApplication.java
@@ -0,0 +1,12 @@
+package com.finalexam.webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebshopApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebshopApplication.class, args);
+	}
+
+}
